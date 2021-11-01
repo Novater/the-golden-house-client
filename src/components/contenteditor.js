4606c774c80d5cd317a8c11a99d762debc9b45fd
Novater <cookiesnmilkjk@gmail.com>
2021-11-01 10:01:24 -0400

    add ability to upload img

--- src/components/contenteditor.js
+++ src/components/contenteditor.js
@@ -16,13 +16,15 @@ export default class ContentEditor extends Component {
           plugins: [
             'advlist autolink lists link image charmap print preview anchor',
             'searchreplace visualblocks code fullscreen',
-            'insertdatetime media table paste code help wordcount code textcolor image'
+            'insertdatetime media table paste code help wordcount code textcolor image paste'
           ],
           toolbar: 'undo redo | formatselect | ' +
           'bold italic forecolor backcolor | alignleft aligncenter ' +
           'alignright alignjustify | bullist numlist outdent indent | ' +
-          'removeformat | code | image | help',
-          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }'
+          'removeformat | code | image | paste | help',
+          content_style: 'body { font-family:Helvetica,Arial,sans-serif; font-size:14px }',
+          images_upload_url: 'postAcceptor.php',
+          file_picker_types: 'file image media'
         }}
         id={`content-${this.props.id}`}
         onEditorChange={this.props.onChange}
